4fcf33684c477077e149ddf9fd5749205c4af5bf
Jean Brito <jeanfbrito@gmail.com>
2017-10-21 17:31:13 -0200

    adjust some views links

--- app/controllers/maintenances_controller.rb
+++ app/controllers/maintenances_controller.rb
@@ -1,10 +1,11 @@
 class MaintenancesController < ApplicationController
   before_action :set_maintenance, only: [:show, :edit, :update, :destroy]
+  before_filter :find_vehicle
 
   # GET /maintenances
   # GET /maintenances.json
   def index
-    @maintenances = Maintenance.all
+    @maintenances = @vehicle.maintenances.order('date desc').page(params[:page])
   end
 
   # GET /maintenances/1
@@ -14,21 +15,22 @@ class MaintenancesController < ApplicationController
 
   # GET /maintenances/new
   def new
-    @maintenance = Maintenance.new
+    @maintenance = @vehicle.maintenances.build
   end
 
   # GET /maintenances/1/edit
   def edit
+    @maintenance = @vehicle.maintenances.find(params[:id])
   end
 
   # POST /maintenances
   # POST /maintenances.json
   def create
-    @maintenance = Maintenance.new(maintenance_params)
+    @maintenance = @vehicle.maintenances.build(maintenance_params)
 
     respond_to do |format|
       if @maintenance.save
-        format.html { redirect_to @maintenance, notice: 'Maintenance was successfully created.' }
+        format.html { redirect_to [@vehicle, :maintenances], notice: 'Maintenance was successfully created.' }
         format.json { render :show, status: :created, location: @maintenance }
       else
         format.html { render :new }
@@ -40,9 +42,10 @@ class MaintenancesController < ApplicationController
   # PATCH/PUT /maintenances/1
   # PATCH/PUT /maintenances/1.json
   def update
+    @maintenance = @vehicle.maintenances.find(params[:id])
     respond_to do |format|
       if @maintenance.update(maintenance_params)
-        format.html { redirect_to @maintenance, notice: 'Maintenance was successfully updated.' }
+        format.html { redirect_to vehicle_maintenances_path(@vehicle), notice: 'Maintenance was successfully updated.' }
         format.json { render :show, status: :ok, location: @maintenance }
       else
         format.html { render :edit }
@@ -70,5 +73,9 @@ class MaintenancesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def maintenance_params
       params.require(:maintenance).permit(:date, :cost, :description, :warranty, :mileage, :vehicle_id, :warranty_status, :type)
+    end
+
+    def find_vehicle
+      @vehicle = Vehicle.friendly.find(params[:vehicle_id])
     end
 end
